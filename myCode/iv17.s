3bb2036e59a38eeadd17f6aab07cfaf8a070ec87
Nathan <nsafran1217@gmail.com>
2022-05-18 20:45:29 -0400

    fix shiftout subs

--- myCode/iv17.s
+++ myCode/iv17.s
@@ -17,6 +17,7 @@ dinPin      = $01
 strobePin   = $02
 clkPin      = $04
 blnkPin     = $08
+gridBit     = $40
 
     .org $4000
 
@@ -26,24 +27,32 @@ InitPortA:
 
 TestOut:
 
-    LD A, $FF
+    
     LD HL, $5000
+    LD A, $44
     LD (HL), A
     INC HL
+    LD A, $aa
     LD (HL), A
     INC HL
+    LD A, $aa
     LD (HL), A
-    INC HL
-    LD (HL), A
-    INC HL
+
+
     LD HL, $5000
     CALL Shift20Bits
+    LD A,'+'
+    CALL OutputChar
 
     LD A, strobePin
     OUT (PortAData), A
     LD A, 0
     OUT (PortAData), A
 
+    LD A,'*'
+    CALL OutputChar
+    
+    
     JP $0036
 
 
@@ -52,12 +61,15 @@ TestOut:
 
 Shift20Bits:                ;Shift out 20 bits at memory location (HL), MSB order
     LD D, 20                ; number of bits to shift out
+    LD E, 8
 
 Shift20BitsLoop:
-    LD A, (HL)              ;get first byte we are going to shift
-    LD B,A
+    LD B, (HL)              ;get first byte we are going to shift
+    ;LD B,A
 Shift8BitsLoop:
     DEC D
+    DEC E
+    LD A, B
     PUSH AF                  ;Store what the byte is that we are working on
     AND %10000000           ;We only want the Most significant bit
     RR A
@@ -66,9 +78,10 @@ Shift8BitsLoop:
     RR A
     RR A
     RR A
-    RR A                      ;Move it over to where Data in is (Bit 0)
+    RR A                    ;Move it over to where Data in is (Bit 0)
+    ;OR 1                    ;TESTING
     OUT (PortAData), A      ;Put out the data so its valid when the clock rises
-    AND clkPin              ;And it with the clk pin bit
+    OR clkPin               ;And it with the clk pin bit
     OUT (PortAData), A      ;Out again with both clock and data
     LD A,0
     OUT (PortAData), A      ;And Out with 0
@@ -76,14 +89,17 @@ Shift8BitsLoop:
     LD A, D                 ;check if we have shifted out 20 bits
     CP 0
     JP Z, DoneShifting20    ;If we did, get out of this
-    LD A,':'
+    LD A,'!'
     CALL OutputChar
-
-    POP AF                   ;Get A back to what we started with
-    RLCA                    ;Rotate it left
-    CP B                    ;Check if it matches what was in HL
+    
+    POP AF
+    RLCA
+    LD B,A
+    LD A,E                  ;get counter of bits
+    CP 0                    ;if its not empty keep loooping
     JP NZ, Shift8BitsLoop   ;If not, keep shifting out
     INC HL                  ;Get next byte to shift out
+    LD E, 8
     JP Shift20BitsLoop
 
 DoneShifting20:

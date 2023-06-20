a68c48f8d0f906ac9a37adf910fb9fd5f6b51808
Nathan <nsafran1217@gmail.com>
2023-06-19 23:47:36 -0400

    basic clock program working

--- myCode/DeviceDemos/displayTimeiv17.s
+++ myCode/DeviceDemos/displayTimeiv17.s
@@ -3,10 +3,12 @@
 ;SS MM HH w DD mm yy 
 ;HH is special becuase this sets 24 or 12 hour and AM or PM
 ;See data sheet
+PortACMD    = $52
+PortAData   = $50
 
 InputChar   = $0035
 OutputChar  = $0038
-PrintStr    = $003B
+;PrintStr    = $003B
 Start       = $0040
 RTCAddress  = $68
 
@@ -15,12 +17,14 @@ RTCAddress  = $68
 Setup:
     CALL sdaOut         ;Note that this sets all pins to out
                         ;This sets us up for the IV17 as well
+    LD A, 0
+    OUT (PortAData), A
 
 ReadTime:
-    LD HL, TimeBuffer   ;memory location to dump to
+    LD HL, TimeBufferVar   ;memory location to dump to
 
 	call stop_i2c		; initiate bus
-	call WAIT_4
+	;call WAIT_4
 	
 	call set_addr_W		; Set address counter to 00h
 
@@ -31,29 +35,87 @@ ReadTime:
 	call putbyte
 	call get_ack
 
-    LD B, 7
-read_time_loop:
-    CALL getbyte
+
+    CALL getbyte    ;sec
 	LD (HL),A
 	INC HL
 	CALL send_ack
-    djnz read_time_loop
 
+    CALL getbyte    ;min
+	LD (HL),A
+	INC HL
+	CALL send_ack
+
+    CALL getbyte    ;hour
+    AND %00011111   ;Strip garbage off
+	LD (HL),A
+	INC HL
+	CALL send_ack
+
+    CALL getbyte    ;Day of Week
+	LD (HL),A
+	INC HL
+	CALL send_ack
+
+    CALL getbyte    ;Day
+	LD (HL),A
+	INC HL
+	CALL send_ack
+
+    CALL getbyte    ;Month
+    AND %00011111   ;Strip garbage off
+	LD (HL),A
+	INC HL
+	CALL send_ack
+
+    CALL getbyte    ;Year
+	LD (HL),A
+	INC HL
+
+	
+    call send_noack ;SEND NOACK WHEN DONE READING
     call stop_i2c
+
+    ;JP Start
+
+
+DisplayTime:
+    LD HL, TimeBufferVar
+    LD B, 3         ;Only do time
+    LD A, 0
+    CALL ShiftOutChar
+    CALL ShiftOutChar
+DisplayTimeLoop: 
+    LD A, (HL)
+    AND %00001111
+    ADD "0"
+    CALL ShiftOutChar
+    LD A, (HL)
+    AND %11110000
+    RRCA
+    RRCA
+    RRCA
+    RRCA
+    ADD "0"
+    CALL ShiftOutChar
+    INC HL
+    djnz DisplayTimeLoop
+    CALL Strobe
+
+    JP ReadTime
     JP Start
 
 
 
 
-
-
-
-
-
     .include i2c.s
-    
-TimeBuffer: .blk 7
+    .include iv17.s
+
+TimeBufferVar: blk 8
+
+    .org $4A00
+    .include iv17ascii.s
+
     ;Padding
     .org $4ffe
-
     .word $0000

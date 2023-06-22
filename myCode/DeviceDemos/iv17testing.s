0940a7bc45c76736240df4598583772cc52d9c9a
Nathan <nsafran1217@gmail.com>
2023-06-21 21:50:56 -0400

    rename iv17 test program, add new tests

--- myCode/DeviceDemos/iv17testing.s
+++ myCode/DeviceDemos/iv17testing.s
@@ -1,14 +1,27 @@
 PortACMD    = $52
 PortAData   = $50
+NumOfVFDTubes = 8
 
 ;Known addresses
+
 InputChar   = $0035
 OutputChar  = $0038
+PrintStr    = $003B
 Start       = $0040
-
+    
+    
+    
     .org $4000
 
     CALL InitPortA
+
+    LD HL, TestMessage
+    CALL ScrollOutString
+    JP Start
+
+    
+
+
 
 
     LD A,"!"
@@ -16,19 +29,29 @@ PrintAllChars:
     CALL ShiftOutChar
     CALL StrobeDisplay
     INC A
-    CP 81
+    CP 126
     JR C, PrintAllChars
 
-
-
 TestOut:
+
+    CALL WAIT_4
+    LD HL, TestMessage
+    CALL ShiftOutStringNULTerm
+    CALL WAIT_4
+    LD HL, TestMessage
+    CALL ScrollOutString
+    JP Start
+
+    
+
     CALL InputChar
     CALL ShiftOutChar
     CALL StrobeDisplay
     JR TestOut
 
-
+TestMessage: .asciiz "Test Long String"
     .include iv17.s
+    .include iv17String.s
     .org $4A00
     .include iv17asciitable.s
     .org $4ffe
